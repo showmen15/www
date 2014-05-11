a37a429db3d47f79dd0268267303592abda8dc4f
showmen15 <showmen15@poczta.onet.pl>
2014-05-11 20:12:35 +0200

    test

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,22 @@
+// ConsoleApplication5.cpp : Defines the entry point for the console application.
+//
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _SCL_SECURE_NO_WARNINGS 1
+
+
+//#include "stdafx.h"
+
+#include <stdio.h>
+
+
+int main(int argc, char* argv[])
+{
+	printf("Program testowy");
+
+	int yyy;
+
+	scanf("%d",&yyy);
+
+	return 0;
+}
+
