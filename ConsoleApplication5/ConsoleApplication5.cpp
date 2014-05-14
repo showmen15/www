9f9de8291e388a49df9afb278732c02377826b0f
showmen15 <showmen15@poczta.onet.pl>
2014-05-14 20:10:54 +0200

    sss

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -7,14 +7,28 @@
 //#include "stdafx.h"
 
 #include <stdio.h>
-
+#include <stdio.h>
+#include <sstream>
+#include <math.h>
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
 	printf("Program testowy");
 
 	int yyy;
+	double X;
 
+	X = (double)rand();
+
+	//X = ((double)rand() / RAND_MAX) * MaxX; 
 
 	scanf("%d",&yyy);
 
